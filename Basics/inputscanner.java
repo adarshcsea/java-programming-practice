d3bfdab7e5814a621ffc26a987ca0d41d8c9b81f
adarshcsea <veerisdollar@gmail.com>
2026-07-31 21:15:36 +0530

    updated code

--- Basics/inputscanner.java
+++ Basics/inputscanner.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 public class inputscanner {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a number :");
         int i = scan.nextInt();
         System.out.println("The number entered :" + i);
+        System.out.println("Enter a character :");
+        char j = scan.next().charAt(0);
+        System.out.println("The character entered :" + j);
         scan.close();
     }
     
