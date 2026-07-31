5f9abb2d5fec5e4d6589da6887353bea025821d5
adarshcsea <veerisdollar@gmail.com>
2026-07-31 21:04:27 +0530

    updated the starbox code

--- Patterns/starbox.java
+++ Patterns/starbox.java
@@ -9,10 +9,10 @@ public class starbox {
         for(int i=n; i>=0; i--){
             for(int j = n; j>=0; j-- ){
                 if(j == 0 || j == m || i == 0 || i == m ){
-                System.out.print("*");
+                System.out.print("* ");
                 }
                 else{
-                    System.out.print(" ");
+                    System.out.print("  ");
                 }
             }
             System.out.println("");
